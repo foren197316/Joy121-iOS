9ee283903bff0b49ba39fe6ebe5bcc4f602ba66b
yc <ycabc1989@163.com>
2014-04-07 15:04:58 +0800

    提交工程

---
+++ Joy/Joy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Joy
+//
+//  Created by 颜超 on 14-4-7.
+//  Copyright (c) 2014年 颜超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Joy/Joy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Joy
+//
+//  Created by 颜超 on 14-4-7.
+//  Copyright (c) 2014年 颜超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
